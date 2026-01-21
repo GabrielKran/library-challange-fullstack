6b8f4bc1c7adb7e101753418390ba460fcf395f2
GabrielKran <gabrielkran@gmail.com>
2026-01-21 08:30:48 -0300

    fix: Ajuste de import

--- backend/src/reservations/reservations.module.ts
+++ backend/src/reservations/reservations.module.ts
@@ -3,8 +3,8 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reservation } from './entities/reservation.entity';
-import { Book } from 'src/books/entities/book.entity';
-import { User } from 'src/users/entities/user.entity';
+import { Book } from '../books/entities/book.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Reservation, Book, User])],
